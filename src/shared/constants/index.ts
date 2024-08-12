95b433ea65d08bb6388e6852b616cfe60058130e
MyStic <khanh.votuan@hcmut.edu.vn>
2024-08-12 23:19:41 +0700

    chore: change to local api for testing

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,8 +1,7 @@
-export const CONDUIT_TOKEN = "conduit-jwt-token"
-export const CONDUIT_USER = "conduit-user"
-export const BASE_URL = "https://api.realworld.io/api"
-export const YOUR_FEED = "Your Feed"
-export const GLOBAL_FEED = "Global Feed"
-export const MY_ARTICLES = "My Articles"
-export const FAVORITED_ARTICLES = "Favorited Articles"
-
+export const CONDUIT_TOKEN = "conduit-jwt-token";
+export const CONDUIT_USER = "conduit-user";
+export const BASE_URL = "http://localhost:8080/api";
+export const YOUR_FEED = "Your Feed";
+export const GLOBAL_FEED = "Global Feed";
+export const MY_ARTICLES = "My Articles";
+export const FAVORITED_ARTICLES = "Favorited Articles";
